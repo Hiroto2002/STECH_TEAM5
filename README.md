b5f5172431a8ed5e0415e98487a0a25718acb785
Hiroto2002 <hiroto2002h14@icloud.com>
2023-09-24 12:29:03 +0900

    feat:動作確認

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 hello!
+
+## 環境構築方法
+- [argo console](https://console.agora.io/)にサインインして、プロジェクト作成
+- .envに環境変数設定
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,95 +1,27 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useState } from 'react'
+import AgoraUIKit from 'agora-react-uikit'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [videoCall, setVideoCall] = useState(true)
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  const rtcProps = {
+    appId: process.env.NEXT_PUBLIC_AGORA_APP_ID as string,
+    channel: process.env.NEXT_PUBLIC_AGORA_CHANNEL as string,
+    token: process.env.NEXT_PUBLIC_AGORA_TOKEN as string,
+  }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+  const callbacks = {
+    EndCall: () => setVideoCall(false),
+  }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  return videoCall ? (
+    <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
+      <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+    </div>
+  ) : (
+    <h3 onClick={() => setVideoCall(true)}>Join</h3>
   )
 }
